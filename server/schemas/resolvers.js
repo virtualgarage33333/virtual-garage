8d3a10f407b3f77d75ed42b434cc6e2263958598
Harry Shah <harvinder.shah@gmail.com>
2021-11-20 17:23:41 -0500

    resolvers and typeDefs updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,108 @@
-// const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
 
 const { User, Item, Order, Comment, Category } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         categories: async () => {
             return Category.findAll();
         },
-    },
+        items: async (parent, { category, name }) => {
+            const params = {};
+      
+            if (category) {
+              params.category = category;
+            }
+      
+            if (name) {
+              params.name = {
+                $regex: name
+              };
+            }
+      
+            return await Item.find(params).populate('category');
+          },
+          item: async (parent, { _id }) => {
+            return await item.findById(_id).populate('category');
+          },
+          user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id).populate({
+                path: 'orders.items',
+                populate: 'category'
+              });
+      
+              user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return user;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
+        order : async (parent, { _id }, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: 'orders.items',
+                    populate: 'category'
+                });
+                return user.orders.id(_id);
+            }
+            throw new AuthenticationError('Not logged in');
+           }
+        },
+        Mutation: {
+            addUser: async (parent, args) => {
+              const user = await User.create(args);
+              const token = signToken(user);
+        
+              return { token, user };
+            },
+            addOrder: async (parent, { products }, context) => {
+              console.log(context);
+              if (context.user) {
+                const order = new Order({ products });
+        
+                await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+        
+                return order;
+              }
+        
+              throw new AuthenticationError('Not logged in');
+            },
+            updateUser: async (parent, args, context) => {
+              if (context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+              }
+        
+              throw new AuthenticationError('Not logged in');
+            },
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+          
+                if (!user) {
+                  throw new AuthenticationError('Incorrect credentials');
+                }
+          
+                const correctPw = await user.isCorrectPassword(password);
+          
+                if (!correctPw) {
+                  throw new AuthenticationError('Incorrect credentials');
+                }
+          
+                const token = signToken(user);
+          
+                return { token, user };
+              },
+            addItem: async (parent, args, context) => {
+                if (context.user) {
+                    const item = await Item.create(args);
+                    await User.findByIdAndUpdate(context.user._id, { $push: { items: item } });
+                    return item;
+                }
+        
+                throw new AuthenticationError('Not logged in');
+              } 
+        }
+        
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,11 +6,44 @@ const typeDefs = gql`
         _id: ID
         categoryName: String
     },
+    type Item {
+        _id: ID
+        itemName: String
+        price: Float
+        category: Category
+    },
+    type Order {
+        _id: ID
+        item: [Item]
+        purchaseDate: String
+    },
 
+    type User {
+        _id: ID
+        username: String
+        email: String
+        city: String
+
+    },
+    type Auth {
+        token: ID
+        user: User
+    },
 
     type Query {
         categories: [Category]
+        items(category: ID, name: String): [Item]
+        item(_id: ID!): Item
+        users: [User]
     }
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addOrder(items: [ID]!): Order
+        updateUser(firstName: String, lastName: String, email: String, password: String): User
+        updateItem(_id: ID!, quantity: Int!): Item
+        login(email: String!, password: String!): Auth
+        addItem(itemName: String!, description: String!, price: Float!, category: ID!): Item
+      }
     `;
 
     // export the  typeDefs;
