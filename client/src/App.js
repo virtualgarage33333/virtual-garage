9547c83c7e4c9d70d7012b90fccf1cd5e2e7a5c0
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-11-21 15:48:08 -0500

    restored homepage

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,17 @@ import React
 from 'react';
 // import { useMutation } from './utils/mutations';
 
-// import Home from "./pages/Home.jsx";
+import Home from "./pages/Home.jsx";
 // import ProductList from "./pages/ProductList";
 // import Product from "./pages/Product";
 // import Cart from "./pages/Cart";
-import AddItem from "./components/AddItem";
+//import AddItem from "./components/AddItem";
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 
 const App = () => {
-  return <AddItem />;
+  return <Home />;
 };
 
 export default App;
