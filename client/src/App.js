0fc448f0c5c2c67a1189a56cea00843093f12e3f
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-11-21 15:43:25 -0500

    debugging front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,16 @@
-import Home from "./pages/Home.jsx";
-import ProductList from "./pages/ProductList";
-//import Product from "./pages/Product";
-//import Cart from "./pages/Cart";
+import React
+//, { useEffect } 
+from 'react';
+// import { useMutation } from './utils/mutations';
+
+// import Home from "./pages/Home.jsx";
+// import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
+// import Cart from "./pages/Cart";
 import AddItem from "./components/AddItem";
-{/* <Route exact path="/success" component={Success} />
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function Success() {
-  const [addOrder] = useMutation(ADD_ORDER);
 
-  useEffect(() => {
-    async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const items = cart.map(item => item._id);
-      
-      if (items.length) {
-        const { data } = await addOrder({ variables: { items } });
-        const itemData = data.addOrder.items;
-    
-        itemData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
-        });
-      }
-        
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
-    }
-
-    saveOrder();
-  }, [addOrder]); */}
 
 const App = () => {
   return <AddItem />;
--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,10 +1,38 @@
 import React, { useEffect } from "react";
 import { useMutation } from '@apollo/react-hooks';
-import Jumbotron from "../components/Jumbotron";
+import Jumbotron from "../components/Jumbotron.jsx";
 import { ADD_ORDER } from "../utils/mutations";
 import { idbPromise } from "../utils/helpers";
 
+
+<Route exact path="/success" component={Success} />
+
 function Success() {
+  const [addOrder] = useMutation(ADD_ORDER);
+
+  useEffect(() => {
+    async function saveOrder() {
+      const cart = await idbPromise('cart', 'get');
+      const items = cart.map(item => item._id);
+      
+      if (items.length) {
+        const { data } = await addOrder({ variables: { items } });
+        const itemData = data.addOrder.items;
+    
+        itemData.forEach((item) => {
+          idbPromise('cart', 'delete', item);
+        });
+      }
+        
+      setTimeout(() => {
+        window.location.assign('/');
+      }, 3000);
+    }
+
+    saveOrder();
+  }, [addOrder]); 
+}
+
     return (
       <div>
         <Jumbotron>
@@ -18,6 +46,5 @@ function Success() {
         </Jumbotron>
       </div>
     );
-  };
 
   export default Success;
