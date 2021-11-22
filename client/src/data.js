23c0e1e12dbd125c1943f3068a4f59abd55ea64a
mmifs <85654339+mmifs@users.noreply.github.com>
2021-11-22 02:39:05 -0500

    edit slider text

--- client/src/data.js
+++ client/src/data.js
@@ -3,26 +3,26 @@ export const sliderInfo = [
     id: 1,
     img: "../assets/images/the-logo.png",
     title: "OUR MISSION",
-    desc: "When it comes to shopping for things like clothes, toys, furniture,and more often times and unfortunately, the go-to tends to bebuying them new. The convenience of running out to a big-box storeand picking up these items can be tempting. But at what cost? Notonly does buying new typically cost you more money, but the environment is also paying a high price to produce those newitems. If you’re new to buying used and are unsure if it’s right,follow the slides.",
+    desc: "When it comes to shopping for things like clothes, toys, furniture, buying brand new items can be expensive. Selections offered at big box stores can be tempting, but buying used can be just as good! Not only is it more expensive to buy new, but the environment is already paying a heavy toll, and reusing goods that are still in decent condition goes a long way!",
   },
 
   {
     id: 2,
     img: "../assets/images/the-logo.png",
-    title: "1. Saving money",
-    desc: "Whether you’re shopping for clothes, furniture, musical instruments, or most anything, you’ll be able to find quality items at lower prices than if you bought them brand new. You can also find name brand clothing, shoes, purses, and more for a fraction of the cost. Who doesn’t want to get designer items at a discount? As an added bonus, by spending less, your dollar goes further.",
+    title: "Saving Money",
+    desc: "Whether you’re shopping for clothes, furniture, musical instruments, or almost anything, you’ll be able to find quality items at lower prices at our Virtual Garage! You can also find name brand clothing, shoes, purses, and more for a reasonable price! Who doesn’t want to get designer items at a discount? By spending less, your dollar goes further.",
   },
   {
     id: 3,
     img: "../assets/images/the-logo.png",
-    title: "2. Helping the environment",
-    desc: "When you think about the resources that go into creating new products, it isn’t just the labor and materials. Energy and natural resources go into creating the packaging that typically accompanies the new product. Each product that is manufactured is responsible for some level of depletion of natural resources. Whether that’s excavating oil, mining metals, clearcutting forests, farming cotton, or pumping water.",
+    title: "Helping the Environment",
+    desc: "When you think about the resources that go into creating new products, it isn’t just the labor and materials. Energy and natural resources go into creating the packaging accumulates over time. That means more garbage, more waste and more pollution. Whether that’s excavating oil, mining metals, clearcutting forests, farming cotton, pumping water or any number of activities, we should mitigate our wear on the environment.",
   },
   {
     id: 4,
     img: "../assets/images/the-logo.png",
-    title: "3. Finding unique items",
-    desc: "Now that you know the big benefits of buying secondhand, it’s time for the really fun part! Half of the fun of thrift shopping, browsing consignment shops, or venturing into a vintage store is the thrill of the hunt. Whether you’re searching for a new outfit to wear for date night, a fun piece of furniture for your house, your next must-read book or games for your kids, you’ll find unique, sometimes hard-to-find items.",
+    title: "Finding Unique Items",
+    desc: "Now that you know the big benefits of buying second-hand, it’s time for the really fun part! Half of the fun of thrift shopping, browsing consignment shops, or venturing into a vintage store is the thrill of the hunt. Whether you’re searching for a new outfit to wear for date night, a fun piece of furniture for your house, your next must-read book or games for your kids, you’ll find unique and interesting items with a story all their own!",
   },
 ];
 
