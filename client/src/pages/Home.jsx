2d2edbde7e51734bc7c2c13b4ac8bd0d44f4f3b7
mmifs <85654339+mmifs@users.noreply.github.com>
2021-11-22 21:58:39 -0500

    troubleshooting

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,53 @@
 import Slider from "../components/Slider.jsx";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Categories from "../components/Categories.jsx";
+import { GET_CATEGORY } from "../utils/queries.js";
+import {UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY} from "../utils/actions.js";
+import { gql, useQuery } from "@apollo/client";
+import { useStoreContext } from '../utils/GlobalState';
+import { idbPromise } from '../utils/helpers';
+
+const foo = gql`
+{
+    categories {
+        _id
+        categoryName
+    }
+}
+`
+
 
 const Home = () => {
+
+const [state, dispatch] = useStoreContext();
+const [currentCategory, setCurrentCategory] = useState({});
+const {loading, error, data, } = useQuery(foo);
+/* useEffect(() => {
+  console.log('data is', data, 'loadingis', loading);
+
+  if (data) {
+    dispatch({
+      type: UPDATE_CATEGORIES,
+      categories: data.categories
+    });
+    data.categories.forEach(category => {
+      idbPromise('categories', 'put', category);
+    });
+  } else if (!loading) {
+    idbPromise('categories', 'get').then(categories => {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categories
+      });
+    });
+  }
+},  [data, loading, dispatch]);*/
+
+useEffect(() => {
+  //refetch()
+  console.log('useeffect data is',data);
+},[loading])
+
   return (
     <div>
       <Slider />
