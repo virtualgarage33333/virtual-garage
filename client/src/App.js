ca03ba8ab85464cfce8079efb0d8701c8dd384a0
Eleonore <eleonore.caclard@gmail.com>
2021-11-24 21:54:50 -0500

    graphql update to original coding line 28

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ import Product from "./pages/Product.jsx";
 //import AddItem from "./components/AddItem";
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: "http://localhost:3001/graphql",
 });
 
 const authenticationLink = setContext((_, { headers }) => {
